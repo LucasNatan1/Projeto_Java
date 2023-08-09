b461d08a15eac456819403d1fed3e3f60d727cba
Lucas Natan <109493001+LucasNatan1@users.noreply.github.com>
2023-08-09 13:02:51 -0300

    Linguagem Técnica de Programação OBD
    A matéria "Linguagem Técnica de Programação Orientada a Objeto utilizando Java" aborda os fundamentos da programação orientada a objetos (POO) utilizando a linguagem de programação Java como meio de instrução. A POO é um paradigma de programação que se concentra na organização do código em torno de objetos, que combinam dados (atributos) e operações (métodos) que podem ser executadas nesses dados. O Java é uma linguagem amplamente utilizada para desenvolvimento de software, conhecida por sua portabilidade e segurança.
    
    Nessa disciplina, os alunos aprendem os conceitos essenciais da POO e como aplicá-los usando a sintaxe do Java. Isso inclui:
    
    Classes e Objetos: Os alunos aprendem a criar classes, que servem como moldes para a criação de objetos. As classes contêm atributos para representar características dos objetos e métodos para definir suas ações.
    
    Encapsulamento: É abordada a ideia de encapsular os detalhes internos de uma classe, tornando certos atributos privados e permitindo o acesso a eles apenas por meio de métodos públicos.
    
    Herança: Os conceitos de herança permitem que uma classe herde atributos e métodos de outra, promovendo a reutilização de código e a criação de hierarquias de classes.
    
    Polimorfismo: Os alunos aprendem a usar o polimorfismo, que permite que diferentes classes se comportem de maneira semelhante, mas com implementações específicas para cada uma.
    
    Interfaces e Abstração: As interfaces definem contratos que as classes devem cumprir, enquanto a abstração envolve a criação de classes base que definem métodos comuns, mas deixam a implementação específica para as subclasses.
    
    Coleções e Estruturas de Dados: Os alunos exploram as diversas estruturas de dados disponíveis no Java, como listas, conjuntos e mapas, e como usá-las para manipular grupos de objetos.
    
    Tratamento de Exceções: A disciplina aborda como lidar com exceções, situações de erro que podem ocorrer durante a execução do programa, e como criar fluxos de código robustos.
    
    Gerenciamento de Memória e Ciclo de Vida de Objetos: É explicado como o Java lida automaticamente com o gerenciamento de memória, incluindo o processo de coleta de lixo, para evitar vazamentos de memória.
    
    Programação Orientada a Aspectos (AOP): Uma introdução à AOP pode ser dada, onde os alunos aprendem a separar preocupações transversais do código principal usando aspectos.
    
    Padrões de Projeto: Pode-se explorar alguns padrões de projeto comuns que ajudam a resolver problemas recorrentes no desenvolvimento de software orientado a objetos.
    
    Em resumo, a matéria "Linguagem Técnica de Programação Orientada a Objeto utilizando Java" oferece aos alunos uma base sólida na programação orientada a objetos usando a linguagem Java, capacitando-os a criar software eficiente, modular e de fácil manutenção, seguindo os princípios da POO.

---
+++ curso/src/br/com/controle/Dobro.java
@@ -0,0 +1,17 @@
+package br.com.controle;
+
+public class Dobro {
+	   private int valor;
+
+	    public int getValor() {
+	        return valor;
+	    }
+
+	    public void setValor(int valor) {
+	        this.valor = valor;
+	    }
+	   public int calcularDobro(){
+	     return this.valor*2;
+	             
+	   }
+	}
